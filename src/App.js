b2fe680d039efd076e370de74269b9619c454cbd
pinar mamat <pnmamat35@gmail.com>
2023-04-11 13:27:29 +0300

    app.js güncellendi

--- src/App.js
+++ src/App.js
@@ -1,23 +1,90 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+
+import Form from './components/Form';
 import './App.css';
 
+import TodoList from './components/TodoList';
+
+
 function App() {
+
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [status, setStatus] = useState("all");
+  const [filteredTodos, setFilteredTodos] = useState([]);
+
+  useEffect(() => {
+    getLocalTodos();
+    
+  }, [])
+
+  
+
+  useEffect(() => {
+    filterHandler(todos);
+    saveLocalTodos()
+    
+    
+    
+
+  }, [todos, status])
+  
+  const filterHandler = () => {
+    switch (status) {
+      case "completed":
+        setFilteredTodos(todos.filter((todo) => todo.completed === true));
+        break;
+      case "uncompleted":
+        setFilteredTodos(todos.filter((todo) => todo.completed === false));
+        break;
+
+      default:
+        setFilteredTodos(todos);
+        break;
+    }
+  }
+
+  //local storage
+
+  const saveLocalTodos = () => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+
+  }
+
+  
+  const getLocalTodos = () => {
+    if (localStorage.getItem("todos") === null ) {
+      localStorage.setItem("todos", JSON.stringify([]));
+
+    }else {
+      setTodos(JSON.parse(localStorage.getItem("todos")));
+    }
+
+  }
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>todos</h1>
+
       </header>
+      <Form
+        inputText={inputText}
+        setInputText={setInputText}
+        todos={todos}
+        setTodos={setTodos}
+        setStatus={setStatus}
+      />
+      <TodoList
+        todos={todos}
+        setTodos={setTodos}
+        filteredTodos={filteredTodos}
+
+      />
+
+
     </div>
   );
 }
